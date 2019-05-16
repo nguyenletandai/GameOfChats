6cf6323c5d08bcbbc88295ed44784c24a327628b
dai <nguyenletandai@gmail.com>
2019-05-17 06:55:24 +0700

    update login

--- GameOfChats/Controller/LoginViewController.swift
+++ GameOfChats/Controller/LoginViewController.swift
@@ -27,9 +27,29 @@ class LoginViewController: UIViewController {
         button.setTitleColor(.white, for: .normal)
         button.translatesAutoresizingMaskIntoConstraints = false
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
-        button.addTarget(self, action: #selector(handleRegister), for: .touchUpInside)
+        button.addTarget(self, action: #selector(handleLoginRegister), for: .touchUpInside)
         return button
     }()
+    
+    @objc func handleLoginRegister() {
+        if loginRegisterSegmentedControl.selectedSegmentIndex == 0 {
+            handleLogin()
+        }else{
+            handleRegister()
+        }
+    }
+    
+    func handleLogin() {
+        guard let email = emailTextField.text,  let password = passwordTextField.text else {print("email hoac pass sai")
+            return
+        }
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            if error != nil {
+                print(error)
+            }
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
     
     @objc func handleRegister() {
         guard let email = emailTextField.text,  let password = passwordTextField.text, let name = nameTextField.text else {print("email hoac pass sai")
@@ -54,6 +74,7 @@ class LoginViewController: UIViewController {
                     return
                 }
                 
+                self.dismiss(animated: true, completion: nil)
                 print("luu du lieu vao database thanh cong")
             })
         }
--- GameOfChats/Controller/ViewController.swift
+++ GameOfChats/Controller/ViewController.swift
@@ -18,10 +18,19 @@ class ViewController: UITableViewController {
 //        ref.updateChildValues(["someValue": 1233123])
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
+        // user is not login in
+        if Auth.auth().currentUser?.uid == nil {
+            perform(#selector(handleLogout), with: nil, afterDelay: 0)
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
     
     @objc func handleLogout() {
+        do {
+            try Auth.auth().signOut()
+        }catch let logoutError {
+            print(logoutError)
+        }
         let loginController = LoginViewController()
         present(loginController, animated: true, completion: nil)
     }
